774cc28ef874ae8a699335ccee5bec7b2555d541
LillyRoxana <78121782+LillyRoxana@users.noreply.github.com>
2022-05-18 14:10:52 -0500

    Create Class Puzzle

---
+++ puzzle.py
@@ -0,0 +1,13 @@
+Class Puzzle:
+    #The word hiding from Jumper
+    # The responsability of Puzzle is... 
+    # Atributes:
+    # _letter(int): The letter of the Puzzle
+    # _
+    
+    def _int_(self):
+      #Constructs a ner Puzzle
+      #Args: 
+      #self(Puzzle): An instance of Puzzle
+      
+      self._letter = #ramdom list, something
