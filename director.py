b480894d146a3893988c734bb31240bb4f408b02
Jesustortoleroh <tor19021@byui.edu>
2022-05-18 19:57:24 -0400

    I Include of responsability of director

--- director.py
+++ director.py
@@ -1,1 +1,37 @@
+from game.jumper import Jumper
+from game.puzzle import Puzzle
+from game.terminal_service import Terminal_service
 
+class Director:
+    
+    """A person who directs the game. 
+    
+    The responsibility of a Director is to control the sequence of play.
+
+    Attributes:
+        jumper (Jumper): The game's jumper.
+        is_playing (boolean): Whether or not to keep playing.
+        puzzle (Puzzle): The game's puzzle.
+        terminal_service: For getting and displaying information on the terminal.
+    """
+    def __init__(self):
+        """Constructs a new Director.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
+        self._jumper = Jumper()
+        self._is_playing = True
+        self._puzzle = Puzzle()
+        self._terminal_service = Terminal_service()
+
+    def start_game(self):
+        """Starts the game by running the main game loop.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
+        while self.keep_playing:
+            self.get_inputs()
+            self.do_updates()
+            self.do_outputs()
