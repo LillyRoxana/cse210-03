0e428df406fc1d06955d98cb442c28b1ac69fb7f
LillyRoxana <78121782+LillyRoxana@users.noreply.github.com>
2022-05-19 14:26:05 -0500

    Update and rename Player.py to Jumper.py

---
+++ game/Jumper.py
@@ -0,0 +1,18 @@
+Class Jumper:
+    """The person who seeks to decipher the Puzzle. 
+    
+    The responsibility of a jumper/player is to keep a record of the letters 
+    he/she has given and the attempts he/she has made (whether these have been correct or incorrect).
+    
+    Attributes:
+        letter (int): The letter proposed by the jumper/player (a-z).
+        attempt (List[int]): The attempts given.
+    """
+   
+    def _int_(self):
+     """Constructs a new Puzzle
+      Args: 
+      self(Puzzle): An instance of Puzzle
+     """
+      self._letter = #ramdom list, something
+      self._attempt = #correct/incorrect, something 
