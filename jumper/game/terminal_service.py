42981a1c87e98ff5d5864ae63a4c013e00d91006
Josh Ausborne <63316430+joshausborne@users.noreply.github.com>
2022-05-19 22:51:46 -0400

    add placeholder method

--- jumper/game/terminal_service.py
+++ jumper/game/terminal_service.py
@@ -1,4 +1,11 @@
 class Terminal_service:
 
+    # TODO 
+    # make the methods/functions actually do stuff
+
     def __init__(self) -> None:
         pass
+
+    def write_text(self,hint):
+        pass
+
