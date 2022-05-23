6b5411efd4d9ea5211a80eaf1b85ca269b179e8b
Lilly Armenta <78121782+LillyRoxana@users.noreply.github.com>
2022-05-23 09:15:38 -0500

    Update puzzle.py. Add del puzzle_attempt

--- jumper/game/puzzle.py
+++ jumper/game/puzzle.py
@@ -19,7 +19,7 @@ class Puzzle:
         self._spaces = list(len(self._letter) * '_')
         self._winner = False
         self._loser = False
-        self._attempt = 5
+        self._attempt = 0
         self._guessing = ""
         
     def get_letter(self, letter):
@@ -28,8 +28,9 @@ class Puzzle:
         Returns:
             letter: The current letter
         """
-        return self._letter
-        
+        #return self._letter
+        return input(letter)
+    
     def show_letters(self):
         """provides/displays the letter indicated/requested
 
@@ -38,15 +39,111 @@ class Puzzle:
             letter (int): The given letter.
         """
         print(self._guessing)
+        
+    def get_try(self):
+        
+        if self._attempt == 0:
+            self.puzzle_attempt1()
+        elif self._attempt == 1:
+            self.puzzle_attempt2()
+        elif self._attempt == 2:
+            self.puzzle_attempt3()
+        elif self._attempt == 3:
+            self.puzzle_attempt4()
+        elif self._attempt == 4:      
+            self.puzzle_attempt5()
+        else self._attempt == 5:       #I am no too sure if is "else" or "elif"
+            self.puzzle_attempt6()
+        
+            
+    def puzzle_attempt1:
+         
+    print()        
+    print("       _____        ")     
+    print("      /_____\       ") 
+    print("      \     /       ")
+    print("       \   /        ")
+    print("        \ /         ")
+    print("         O          ")
+    print("        /|\         ")
+    print("        / \         ")
+    print("   ^^^^^^^^^^^^^    ")
+           
+    def puzzle_attempt2:
+        
+    print()        
+    print("                    ")     
+    print("      /_____\       ") 
+    print("      \     /       ")
+    print("       \   /        ")
+    print("        \ /         ")
+    print("         O          ")
+    print("        /|\         ")
+    print("        / \         ")
+    print("   ^^^^^^^^^^^^^    ")    
+        
+    def puzzle_attempt3:
+        
+    print()        
+    print("                    ")     
+    print("                    ") 
+    print("      \     /       ")
+    print("       \   /        ")
+    print("        \ /         ")
+    print("         O          ")
+    print("        /|\         ")
+    print("        / \         ")
+    print("   ^^^^^^^^^^^^^    ")   
+    
+    def puzzle_attempt4:
+        
+    print()        
+    print("                    ")     
+    print("                    ") 
+    print("                    ")
+    print("       \   /        ")
+    print("        \ /         ")
+    print("         O          ")
+    print("        /|\         ")
+    print("        / \         ")
+    print("   ^^^^^^^^^^^^^    ")       
+        
+    
+    def puzzle_attempt5:
+    
+    print()        
+    print("                    ")     
+    print("                    ") 
+    print("                    ")
+    print("                    ")
+    print("        \ /         ")
+    print("         O          ")
+    print("        /|\         ")
+    print("        / \         ")
+    print("   ^^^^^^^^^^^^^    ")  
 
+    def puzzle_attempt6:
+    
+    print()        
+    print("                    ")     
+    print("                    ") 
+    print("                    ")
+    print("                    ")
+    print("                    ")
+    print("         X          ")
+    print("        /|\         ")
+    print("        / \         ")
+    print("   ^^^^^^^^^^^^^    ")  
+
+    
     # TODO
     # make the methods/functions actually do stuff
 
-    def watch_jumper(self,blah):
-        pass
+    #def watch_jumper(self,blah):
+        #pass
 
-    def get_hint(self):
-        pass
+    #def get_hint(self):
+        #pass
 
-    def is_found(self):
-        pass
+    #def is_found(self):
+     #   pass
